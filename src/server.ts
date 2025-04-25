927211667d7ac93d00d5f72097c8da7044287294
Chathura Prasanga <chathuraprasanga98@gmail.com>
2025-04-26 00:39:24 +0530

    initial commit

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ const port = process.env.PORT ?? 5000;
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
